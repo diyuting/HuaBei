4ff1d79dcfee6961f57df2de69f790c35b16787d
diyuting <78398832+diyuting@users.noreply.github.com>
2021-09-23 19:48:15 +0800

    diyuting

---
+++ src/main/java/com/example/huabei/HuabeiApplication.java
@@ -0,0 +1,13 @@
+package com.example.huabei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuabeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuabeiApplication.class, args);
+    }
+
+}
